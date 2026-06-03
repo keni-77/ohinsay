bbc2eba243b3c15c8bd8a1b2fcc20e5684e89ece
keni-77 <kenikeni2024@outlook.com>
2026-06-03 12:14:56 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
 // js/main.js
 import { transpileToCpp } from './transpiler.js';
+import {
+  localSignUp,
+  localSignIn,
+  localLogout,
+  saveCodeLocal,
+  loadLocalCodes,
+  loadSelectedCode,
+  deleteCodeLocal,
+  showLoggedInUI,
+  currentUser
+} from './storage.js';
 
-window.runCode = async function () {
+// HTML から呼べるように window に公開
+window.runCode = runCode;
+window.localSignUp = localSignUp;
+window.localSignIn = localSignIn;
+window.localLogout = localLogout;
+window.saveCodeLocal = saveCodeLocal;
+window.loadLocalCodes = loadLocalCodes;
+window.loadSelectedCode = loadSelectedCode;
+window.deleteCodeLocal = deleteCodeLocal;
+
+async function runCode() {
     const output = document.getElementById('output');
     output.innerText = "実行中...";
 
@@ -32,4 +53,4 @@ window.runCode = async function () {
     } catch (err) {
         output.innerText = "通信エラー: " + err.message;
     }
-};
+}
